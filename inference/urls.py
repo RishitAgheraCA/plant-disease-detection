e631901553cd485c0aea32841449272088d9997a
RishitAgheraCA <111581641+RishitAgheraCA@users.noreply.github.com>
2022-11-22 20:56:58 -0500

    Update urls.py

--- inference/urls.py
+++ inference/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-
+from django.views.generic.base import RedirectView
 from inference.views import InferenceView
 
 urlpatterns = [
     path('', InferenceView.as_view(), name='index'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
